f098f11309c85955eaff1ed1560b80d64ef0a809
Synbulat Biishev <62868468+Syjalo@users.noreply.github.com>
2021-06-01 19:56:14 +0500

    Bug fix

--- src/commands/utility/say.js
+++ src/commands/utility/say.js
@@ -2,15 +2,16 @@ module.exports = {
   name: 'say',
   permsWhitelist: ['ADMINISTRATOR'],
   execute(message, args, client) {
-    const channelID = args[0].replace(/[<@!&#>\\]/g, '')
+    const targetChannelID = args[0].replace(/[<@!&#>\\]/g, '')
+    const targetChannel = client.channels.resolve(targetChannelID)
     args.shift()
 
-    if(message.guild.id !== channel?.guild.id && !client.isOwner(message)) {
+    if(message.guild.id !== targetChannel?.guild.id && !client.isOwner(message)) {
       message.reply(client.getString('say.anotherServer', { locale: message }))
       return
     }
 
     if(message.deletable) message.delete()
-    if(channel) channel.send(args.join(' '))
+    if(targetChannel) targetChannel.send(args.join(' '))
   }
 }
