81c71469e44fe02ebe58ee79e8b3572640a852d9
Synbulat Biishev <62868468+Syjalo@users.noreply.github.com>
2021-06-08 00:03:20 +0500

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,13 @@
+const Discord = require('discord.js')
+
 module.exports = {
   name: 'ready',
   once: true,
   execute(client) {
     console.log('Ready!')
+    const embed = new Discord.MessageEmbed()
+    .setTitle('Ready!')
+    .setColor(client.constants.greenColor)
+    client.owner.send(embed)
   }
 }
