3785a3efd0d80fc85167a4acebe14baea761592b
Synbulat Biishev <62868468+Syjalo@users.noreply.github.com>
2021-06-08 00:03:33 +0500

    Add partials

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const Discord = require('discord.js')
 
 const ABClient = require('./utils/ABClient')
 
-const client = new ABClient({ intents: Discord.Intents.ALL })
+const client = new ABClient({ partials: ["USER", "CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION"], intents: Discord.Intents.ALL })
 
 client.login()
