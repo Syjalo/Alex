3d3cd9c51f2430b55bf01fd8820cfb1d0b033210
Synbulat Biishev <62868468+Syjalo@users.noreply.github.com>
2021-06-18 23:43:44 +0500

    Bug fix

--- src/events/ready.js
+++ src/events/ready.js
@@ -6,12 +6,12 @@ module.exports = {
   async execute(client) {
     await client.application.fetch()
     console.log('Ready!')
-    if(process.env.PROCESS === 'production') {
+    if(process.env.PROCESS !== 'production') {
       const embed = new Discord.MessageEmbed()
       .setTitle('Ready!')
       .setTimestamp(client.readyAt)
       .setColor(client.constants.Colors.green)
-      client.owner.send({ embeds: embed })
+      client.owner.send({ embeds: [embed] })
     }
   }
 }
