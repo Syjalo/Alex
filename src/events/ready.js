e64ff279de458f653c2cde9633c6bcf9946039bc
Synbulat Biishev <62868468+Syjalo@users.noreply.github.com>
2021-06-23 20:55:12 +0500

    Some changes

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,11 +4,14 @@ module.exports = {
   name: 'ready',
   once: true,
   async execute(client) {
-    await client.application.fetch()
     client.mainGuild = client.guilds.cache.get(process.env.MAIN_GUILD_ID)
     client.altDetectorChannel = client.channels.cache.get(process.env.ALT_DETECTOR_CHANNEL_ID)
+    client.suggestionsChannel = client.channels.cache.get(process.env.SUGGESTIONS_CHANNEL_ID)
+    
+    await client.application.fetch()
     await client.mainGuild.commands.fetch()
     await client.commands.setup()
+    
     console.log('Ready!')
     if(process.env.PROCESS === 'production') {
       const embed = new Discord.MessageEmbed()
