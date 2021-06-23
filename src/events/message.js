47f0c208198cbe96cf7296f8c3c4777f90c553c4
Synbulat Biishev <62868468+Syjalo@users.noreply.github.com>
2021-06-23 20:58:12 +0500

    Bug fix

--- src/events/message.js
+++ src/events/message.js
@@ -2,8 +2,8 @@ module.exports = {
   name: 'message',
   async execute(message, client) {
     if (message.channel.id === client.suggestionsChannel.id) {
-      await message.react('<:allowed:857336659465076737>')
-      await message.react('<:denied:857336659619348540>')
+      await message.react('857336659465076737')
+      await message.react('857336659619348540')
     }
   }
 }
