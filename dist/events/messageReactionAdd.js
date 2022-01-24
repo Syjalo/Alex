eb51f891ca25b4d687e989ae3ce8ceb34be33b3d
Synbulat Biishev <syjalo.dev@gmail.com>
2022-01-24 20:14:23 +0500

    Add dist to gitignore

--- dist/events/messageReactionAdd.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Constants_1 = require("../util/Constants");
-exports.default = (client) => {
-    client.on('messageReactionAdd', (reaction, user) => {
-        if (reaction.message.channel.id === Constants_1.Ids.channels.suggestions && reaction.message.author.id === user.id)
-            reaction.users.remove(user.id);
-    });
-};
