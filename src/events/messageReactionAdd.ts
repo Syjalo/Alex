9e47225bd07f41e0165c9c456e11ffa1d1185214
Synbulat Biishev <syjalo.dev@gmail.com>
2022-01-25 08:20:10 +0500

    fix: await fetching

--- src/events/messageReactionAdd.ts
+++ src/events/messageReactionAdd.ts
@@ -3,7 +3,7 @@ import { Ids } from '../util/Constants';
 
 export default (client: AlexClient) => {
   client.on('messageReactionAdd', async (reaction, user) => {
-    if (reaction.message.partial) reaction.message.fetch();
+    if (reaction.message.partial) await reaction.message.fetch();
     if (user.partial) await user.fetch();
     if (reaction.message.channel.id === Ids.channels.suggestions && reaction.message.author!.id === user.id)
       reaction.users.remove(user.id);
