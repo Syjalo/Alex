f793d22dbd47259d3b0eb24305385e9e65b0d0a0
Synbulat Biishev <syjalo.dev@gmail.com>
2022-01-25 08:11:34 +0500

    fix: user may be partial

--- src/events/messageReactionAdd.ts
+++ src/events/messageReactionAdd.ts
@@ -2,7 +2,8 @@ import { AlexClient } from '../util/AlexClient';
 import { Ids } from '../util/Constants';
 
 export default (client: AlexClient) => {
-  client.on('messageReactionAdd', (reaction, user) => {
+  client.on('messageReactionAdd', async (reaction, user) => {
+    if (user.partial) await user.fetch();
     if (reaction.message.channel.id === Ids.channels.suggestions && reaction.message.author!.id === user.id)
       reaction.users.remove(user.id);
   });
