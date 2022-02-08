f5f4224783dcc8e74f215509234e43493589d66b
Synbulat Biishev <syjalo.dev@gmail.com>
2022-02-08 16:25:20 +0500

    fix: message can be automatically deleted

--- src/events/messageReactionAdd.ts
+++ src/events/messageReactionAdd.ts
@@ -3,7 +3,8 @@ import { ids } from '../util/Constants';
 
 export default (client: AlexClient) => {
   client.on('messageReactionAdd', async (reaction, user) => {
-    if (reaction.message.partial) await reaction.message.fetch();
+    if (reaction.message.partial) await reaction.message.fetch().catch(() => null);
+    if (!reaction.message) return;
     if (user.partial) await user.fetch();
     if (reaction.message.channel.id === ids.channels.suggestions && reaction.message.author!.id === user.id)
       reaction.users.remove(user.id);
