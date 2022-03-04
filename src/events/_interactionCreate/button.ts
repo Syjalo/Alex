8ae1940cf1fe717162f97ce0e7b9218e46db4a3a
Synbulat Biishev <syjalo.dev@gmail.com>
2022-03-04 23:36:22 +0500

    fix: button deteting

--- src/events/_interactionCreate/button.ts
+++ src/events/_interactionCreate/button.ts
@@ -5,18 +5,17 @@ import { AlexClient } from '../../util/AlexClient';
 
 export default async (interaction: ButtonInteraction<'cached'>, client: AlexClient) => {
   const [action, data] = interaction.customId.split(':');
-  if (action.startsWith('hostname')) {
+  if (action.startsWith('hostname') || action.startsWith('user-report-resolve')) {
     if (action === 'hostname-allow') {
       await client.db.hostnames.updateOne({ _id: new ObjectId(data) }, { $set: { status: HostnameStatus.Allowed } });
       interaction.reply({ content: 'This link has been successfully allowed', ephemeral: true });
     } else if (action === 'hostname-deny') {
       await client.db.hostnames.updateOne({ _id: new ObjectId(data) }, { $set: { status: HostnameStatus.Denied } });
       interaction.reply({ content: 'This link has been successfully denied', ephemeral: true });
-    } else if (action === 'user-report-resolve') {
-      interaction.message.delete().catch(() => null);
+    }if (action === 'user-report-resolve') {
       interaction.reply({ content: 'This report resolved' });
     }
-    await interaction.message.delete();
+    await interaction.message.delete().catch(() => null);
     return;
   }
 };
