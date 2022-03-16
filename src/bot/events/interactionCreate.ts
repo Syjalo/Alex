09b7a5c96155bbef70c1a9b1c62fc53df5365e70
Synbulat Biishev <syjalo.dev@gmail.com>
2022-03-17 00:27:19 +0500

    fix: strings path fix

--- src/bot/events/interactionCreate.ts
+++ src/bot/events/interactionCreate.ts
@@ -20,12 +20,12 @@ export default (client: AlexClient) => {
           variables,
         } = options;
         locale = Util.resolveLocale(locale);
-        let enStrings = require(`../../strings/en-US/${fileName}`);
+        let enStrings = require(`../../../strings/en-US/${fileName}`);
         let strings: Record<string, any>;
         try {
-          strings = require(`../../strings/${locale}/${fileName}`);
+          strings = require(`../../../strings/${locale}/${fileName}`);
         } catch {
-          strings = require(`../../strings/en-US/${fileName}`);
+          strings = require(`../../../strings/en-US/${fileName}`);
         }
 
         key.split('.').forEach((keyPart) => {
