b1d4be1fc228c055972446d7261cfc5fa13d7cf8
Synbulat Biishev <syjalo.dev@gmail.com>
2022-03-31 00:11:51 +0500

    fix(selectMenu): defer reply

--- src/bot/events/_interactionCreate/selectMenu.ts
+++ src/bot/events/_interactionCreate/selectMenu.ts
@@ -4,20 +4,21 @@ import { Ids } from '../../util/Constants';
 
 export const selectMenu = async (interaction: SelectMenuInteraction<'cached'>, getString: GetString) => {
   if (interaction.customId === 'language-role') {
+    await interaction.deferReply({ ephemeral: true });
+
     const languageRoles = [Ids.roles.english, Ids.roles.russian, Ids.roles.otherLanguages],
       rolesToRemove = languageRoles.filter((roleId) => !interaction.values.includes(roleId));
 
     await interaction.member.roles.add(interaction.values);
     await interaction.member.roles.remove(rolesToRemove);
 
-    await interaction.reply({
+    await interaction.editReply({
       content: getString('languageRolesUpdated', {
         variables: {
           count: interaction.values.length,
           roles: interaction.values.map((value) => `<@&${value}>`).join(', '),
         },
       }),
-      ephemeral: true,
     });
   }
 };
