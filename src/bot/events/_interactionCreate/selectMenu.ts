82a01c49247e854086dc85edbe3422b00fe86168
Synbulat Biishev <syjalo.dev@gmail.com>
2022-04-10 11:26:09 +0500

    feat: add link to russian server

--- src/bot/events/_interactionCreate/selectMenu.ts
+++ src/bot/events/_interactionCreate/selectMenu.ts
@@ -6,10 +6,11 @@ export const selectMenu = async (interaction: SelectMenuInteraction<'cached'>, g
   if (interaction.customId === 'language-role') {
     await interaction.deferReply({ ephemeral: true });
 
-    const languageRoles = [Ids.roles.english, Ids.roles.russian, Ids.roles.otherLanguages],
+    const languageRoles = [Ids.roles.english, Ids.roles.otherLanguages],
+      rolesToAdd = languageRoles.filter((roleId) => interaction.values.includes(roleId)),
       rolesToRemove = languageRoles.filter((roleId) => !interaction.values.includes(roleId));
 
-    await interaction.member.roles.add(interaction.values);
+    await interaction.member.roles.add(rolesToAdd);
     await interaction.member.roles.remove(rolesToRemove);
 
     await interaction.editReply({
@@ -20,5 +21,12 @@ export const selectMenu = async (interaction: SelectMenuInteraction<'cached'>, g
         },
       }),
     });
+
+    if (interaction.values.includes(Ids.roles.russian)) {
+      await interaction.followUp({
+        content: 'Вот ссылка на русский сервер: https://discord.gg/aA2MM4XgM8',
+        ephemeral: true,
+      });
+    }
   }
 };
